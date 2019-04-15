454d756fe5901ac41ae82853e79c28c748c556de
genchik1 <geno.matveev@gmail.com>
2019-04-15 23:49:06 +0300

    1

--- code/graphs7.py
+++ code/graphs7.py
@@ -11,10 +11,12 @@ import math
 import pprint
 from scipy.interpolate import interp1d, splrep, splev
 
+from shapely import *
+
 
 def calc_E1(a1, a2, E, h, v, d, kx, ky):
     f = (1-a1*a2)*(E/h*v)+(a2-a1)*ky - (a1+a2)*kx*(1/np.tan(kx*d))
-    return kx, ky, f
+    return f
 
 
 def calc_E2(a1, a2, E, h, v, d, kx, ky):
@@ -23,11 +25,11 @@ def calc_E2(a1, a2, E, h, v, d, kx, ky):
 
 
 if __name__ == '__main__':
-    a1 = 1#  Symbol('a_1')
-    a2 = 0#  Symbol('a_2')
+    a1 = .4#  Symbol('a_1')
+    a2 = .6#  Symbol('a_2')
     h = 1   #Symbol('h')
     v = 1   #Symbol('v')
-    d = 1   #Symbol('d')
+    d = 2   #Symbol('d')
 
     # n = 5*np.pi
 
@@ -37,28 +39,33 @@ if __name__ == '__main__':
     ky = np.linspace(-n, n, 1000)
 
 
-    E1 = np.sqrt((h**2)*(v**2)*(kx**2+ky**2))
-    E2 = np.sqrt((h**2)*(v**2)*(-(kx**2)+ky**2))
 
     E0 = v*h*ky
 
-    _kx, _ky, _E = calc_E1(a1, a2, E1, h, v, d, kx, ky)
 
-    df = pd.DataFrame({
-        '_kx':_kx,
-        '_ky':_ky,
-        '_E':_E,
-    })
+    result = []
+
+
+    for k_y in ky:
+        for k_x in [1]:
+            f2 = np.sqrt((h**2)*(v**2)*(k_x**2+k_y**2))
+            f1 = calc_E1(a1, a2, f2, h, v, d, k_x, k_y)
+
+            f3 = np.sqrt((h**2)*(v**2)*(-k_x**2+k_y**2))
+            f4 = calc_E2(a1, a2, f3, h, v, d, k_x, k_y)
+            result.append({'kx':k_x, 'ky':k_y, 'E1':f1, 'E2':f4})
+
+    df = pd.DataFrame(result)
     
-    # data = pd.DataFrame({'ky':ky,
-    #                      'E1':,
-    #                      'E2':calc_E2(a1, a2, E2, h, v, d, -kx, ky),
-    #                      'E01':E0,
-    #                      'E02':-E0})
-
-
-
-    print (df.head())
     print (len(df))
 
-    # data.to_csv('e.csv', sep='\t', index=None, header=None)
+    df.to_csv('e.csv', sep='\t', index=None, header=None)
+
+    # df['n'] = df.apply(lambda x: np.abs(np.abs(x['E1'])-np.abs(x['E2'])), axis=1)
+    # df = df[df['n']<.2]
+
+
+    # df.to_csv('e2.csv', sep='\t', index=None, header=None)
+
+    df['point'] = list(zip(df['k_y'], df['E1']))
+    print (df.head())
